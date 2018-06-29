8217eabcae2f899b23a95c832b1042f2ba2f9220
Kelvin Kitika <kelvin.kitika@gmail.com>
2018-06-29 13:15:37 +0300

    Updating Readme.md

---
+++ designs.js
@@ -0,0 +1,26 @@
+// Select color input
+// Select size input
+
+$('#sizepicker').submit(function (event){
+    // To prevent the page from reloading
+    event.preventDefault();
+    // To remove the previous table created
+    $('#pixelCanvas').empty();
+    //To run the function when clicked
+    makeGrid()
+});
+
+function makeGrid(){
+    const rows = $('#inputHeight').val();
+    const columns = $('#inputWeight').val();
+    //To create rows based on the entries
+    for (let i = 0; i < rows; i++){
+        $('#pixelCanvas').append('<tr></tr>');
+    }
+    //To modify the columns based on the
+    for (let j = 0; j < columns; j++){
+        $('tr').append('<td></td>');
+    }
+}
+
+
